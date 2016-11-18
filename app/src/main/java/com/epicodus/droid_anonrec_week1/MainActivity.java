9f1437be9f2afea2146c7b9b58fc4eb0d1809487
Eric Krause <krause197@gmail.com>
2016-11-18 15:05:22 -0800

    Click listener to welcome button with toast.  Add butterknife dependancy to build.gradle

--- app/src/main/java/com/epicodus/droid_anonrec_week1/MainActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/MainActivity.java
@@ -2,12 +2,25 @@ package com.epicodus.droid_anonrec_week1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button mWelcomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
+            mWelcomeButton.setOnCLickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "One Day At A Time", Toast.LENGTH_LONG).show();
+                }
+            })
     }
 }
