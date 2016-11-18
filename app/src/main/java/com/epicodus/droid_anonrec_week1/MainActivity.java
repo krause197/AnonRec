770eeac961d215a35e3bed145e0bd5ecdf011f5d
Eric Krause <krause197@gmail.com>
2016-11-18 15:24:35 -0800

    create new Register Activity and bind it through butterknife to welcome button

--- app/src/main/java/com/epicodus/droid_anonrec_week1/MainActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/MainActivity.java
@@ -1,26 +1,34 @@
 package com.epicodus.droid_anonrec_week1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button mWelcomeButton;
+    @Bind(R.id.welcomeButton) Button mWelcomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mWelcomeButton = (Button) findViewById(R.id.welcomeButton);
-            mWelcomeButton.setOnCLickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "One Day At A Time", Toast.LENGTH_LONG).show();
-                }
-            })
+        mWelcomeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "One Day At A Time", Toast.LENGTH_LONG).show();
+        if (v == mWelcomeButton) {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+
+        }
     }
 }
