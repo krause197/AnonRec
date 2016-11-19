efffa4fd090364623ade6175e6bb7e72ffbf1083
Eric Krause <krause197@gmail.com>
2016-11-18 16:52:49 -0800

    Bind edittext imputs from register activity and submit button  using butterknife

--- app/src/main/java/com/epicodus/droid_anonrec_week1/RegisterActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/RegisterActivity.java
@@ -2,12 +2,31 @@ package com.epicodus.droid_anonrec_week1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.homeGroupEditText2) EditText mHomeGroupEditText2;
+    @Bind(R.id.neighborhoodEditText) EditText mNeighborhoodEditText;
+    @Bind(R.id.submitButton) Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        mSubmitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSubmitButton) {
+
+        }
     }
 }
