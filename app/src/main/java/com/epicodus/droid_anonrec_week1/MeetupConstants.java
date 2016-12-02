c16b764808681fb981e9098a32a64d0f261b9477
Eric Krause <krause197@gmail.com>
2016-12-02 14:54:32 -0800

    Add Meetup API Key to gradle.properties, initialize string constants, connect credentials in build.gradle

---
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/MeetupConstants.java
@@ -0,0 +1,8 @@
+package com.epicodus.droid_anonrec_week1;
+
+/**
+ * Created by Guest on 12/2/16.
+ */
+public class MeetupConstants {
+    public static final String MEETUP_TOKEN = BuildConfig.MEETUP_TOKEN;
+}
