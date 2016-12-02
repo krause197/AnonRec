4dccc94dd240cfa0ee5ad6a74b0d92315243171a
Eric Krause <krause197@gmail.com>
2016-12-02 14:33:38 -0800

    set up basic MessageBoard Activity layout with underconstruction message and Back button

--- app/src/main/java/com/epicodus/droid_anonrec_week1/MessageActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/MessageActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.droid_anonrec_week1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MessageActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class MessageActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.backButton)
+    Button mbackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        mbackButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mbackButton) {
+            Intent intent = new Intent(MessageActivity.this, HomePageActivity.class);
+            startActivity(intent);
+        }
     }
 }
