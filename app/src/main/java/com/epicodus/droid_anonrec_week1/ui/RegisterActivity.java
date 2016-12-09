24561bef0667953e4630710121f8047edfba2cd7
Eric Krause <krause197@gmail.com>
2016-12-09 10:04:47 -0800

    start modification to registerActivity to create user in firebase, authenticate.  Also update register layout for password fields and link to log in page

--- app/src/main/java/com/epicodus/droid_anonrec_week1/ui/RegisterActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/ui/RegisterActivity.java
@@ -6,24 +6,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.epicodus.droid_anonrec_week1.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPassEditText) EditText mConfirmPassEditText;
     @Bind(R.id.homeGroupEditText2) EditText mHomeGroupEditText2;
     @Bind(R.id.neighborhoodEditText) EditText mNeighborhoodEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
     @Bind(R.id.submitButton) Button mSubmitButton;
 
-    @Override
+    public static final String TAG = RegisterActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        mLoginTextView.setOnClickListener(this);
         mSubmitButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
