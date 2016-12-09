96b8b17ef8ac42d1ddf91fce76b44230613d4cfe
Eric Krause <krause197@gmail.com>
2016-12-09 10:26:43 -0800

    finish registerActivity modification to authenticate new users

--- app/src/main/java/com/epicodus/droid_anonrec_week1/ui/RegisterActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/ui/RegisterActivity.java
@@ -1,21 +1,28 @@
 package com.epicodus.droid_anonrec_week1.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.droid_anonrec_week1.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
-    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.userNameEditText) EditText mUserNameEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.confirmPassEditText) EditText mConfirmPassEditText;
     @Bind(R.id.homeGroupEditText2) EditText mHomeGroupEditText2;
@@ -53,13 +60,52 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     }
 
     @Override
-    public void onClick(View v) {
-        if (v == mSubmitButton) {
-            Intent intent = new Intent(RegisterActivity.this, ProfileActivity.class);
-            intent.putExtra("name", mNameEditText.getText().toString());
-            intent.putExtra("homegroup", mHomeGroupEditText2.getText().toString());
-            intent.putExtra("neighborhood", mNeighborhoodEditText.getText().toString());
-            startActivity(intent);
+    public void onClick(View view) {
+        if (view == mSubmitButton) {
+            createNewUser();
         }
+        if (view == mLoginTextView ) {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void createNewUser() {
+        final String username = mUserNameEditText.getText().toString().trim();
+        final String homeGroup = mHomeGroupEditText2.getText().toString().trim();
+        final String neighborhood = mNeighborhoodEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPass = mConfirmPassEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication Successful");
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 }
