6fc6797b7c28a872868a2e2f899f24a39d686bd0
Eric Krause <krause197@gmail.com>
2016-12-09 10:47:13 -0800

    create loginActivity class and layout

--- app/src/main/java/com/epicodus/droid_anonrec_week1/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/ui/LoginActivity.java
@@ -1,13 +1,122 @@
 package com.epicodus.droid_anonrec_week1.ui;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.epicodus.droid_anonrec_week1.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static android.view.View.*;
+
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
+    public static final String TAG = LoginActivity.class.getSimpleName();
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.userNameEditText) EditText mUserNameEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        mLoginTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+
+        createAuthProgressDialog();
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Authenticating User Profile");
+        mAuthProgressDialog.setMessage("Serenity is not the absence of conflict, but the ability to cope with it ...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mLoginButton) {
+            loginWithPassword();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void loginWithPassword() {
+        String username = mUserNameEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (username.equals("")) {
+            mUserNameEditText.setError("Please enter your User Name.");
+            return;
+        }
+        mAuthProgressDialog.show();
+        if (password.equals("")) {
+            mPasswordEditText.setError("Please enter your Password");
+        }
+
+        mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mAuthProgressDialog.dismiss();
+                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInWithUserName", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication Failed, Please Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
