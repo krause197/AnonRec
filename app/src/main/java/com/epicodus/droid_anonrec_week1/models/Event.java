1fa74a8156dcae425ca1290cdd1407de83f8e0bb
Eric Krause <krause197@gmail.com>
2016-12-13 03:29:46 -0800

    removed m variables from event class to be saved into firebase

--- app/src/main/java/com/epicodus/droid_anonrec_week1/models/Event.java
+++ app/src/main/java/com/epicodus/droid_anonrec_week1/models/Event.java
@@ -7,35 +7,35 @@ import org.parceler.Parcel;
  */
 @Parcel
 public class Event {
-    private String mName;
-    private String mDateTimeGroup;
-    private String mEvent_Url;
-    private String mFullAddress;
-    private String mYes_rsvp_count;
-    private String mMaybe_rsvp_count;
-    private String mGroup_name;
-    private String mWho;
+    String name;
+    String dateTimeGroup;
+    String event_Url;
+    String fullAddress;
+    String yes_rsvp_count;
+    String maybe_rsvp_count;
+    String group_name;
+    String who;
 
     public Event() {}
 
     public Event (String name, String dateTimeGroup, String event_url, String fullAddress, String yes_rsvp_count, String maybe_rsvp_count, String group_name, String who) {
-        this.mName = name;
-        this.mDateTimeGroup = dateTimeGroup;
-        this.mEvent_Url = event_url;
-        this.mFullAddress = fullAddress;
-        this.mYes_rsvp_count = yes_rsvp_count;
-        this.mMaybe_rsvp_count = maybe_rsvp_count;
-        this.mGroup_name = group_name;
-        this.mWho = who;
+        this.name = name;
+        this.dateTimeGroup = dateTimeGroup;
+        this.event_Url = event_url;
+        this.fullAddress = fullAddress;
+        this.yes_rsvp_count = yes_rsvp_count;
+        this.maybe_rsvp_count = maybe_rsvp_count;
+        this.group_name = group_name;
+        this.who = who;
     }
 
-    public String getName() { return mName;}
-    public String getDateTimeGroup() { return mDateTimeGroup;}
-    public String getEvent_Url() { return mEvent_Url;}
-    public String getAddress() { return mFullAddress;}
-    public String getYes_rsvp_count() { return mYes_rsvp_count;}
-    public String getMaybe_rsvp_count() { return mMaybe_rsvp_count;}
-    public String getGroup_name() { return mGroup_name;}
-    public String getWho() { return mWho;}
+    public String getName() { return name;}
+    public String getDateTimeGroup() { return dateTimeGroup;}
+    public String getEvent_Url() { return event_Url;}
+    public String getAddress() { return fullAddress;}
+    public String getYes_rsvp_count() { return yes_rsvp_count;}
+    public String getMaybe_rsvp_count() { return maybe_rsvp_count;}
+    public String getGroup_name() { return group_name;}
+    public String getWho() { return who;}
 
 }
