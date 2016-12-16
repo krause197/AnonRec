fa7180ec519abb051056c79dc3f7d49732a9011d
Eric Krause <krause197@gmail.com>
2016-12-16 08:26:48 -0800

    create additional util files, onstartdraglistener and simpleitemtouchhelpercallback

---
+++ app/src/main/java/com/epicodus/anonrec/util/OnStartDragListener.java
@@ -0,0 +1,10 @@
+package com.epicodus.anonrec.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Guest on 12/16/16.
+ */
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
